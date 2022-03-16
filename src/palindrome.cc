bece48f318137334579b8f053f13dac0da7f5e09
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-03-16 11:04:29 +0900

    palindrome.

---
+++ src/palindrome.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool is_palindrome(string &s) {
+  int n = static_cast<int>(s.size());
+  // a b c -> 0
+  // a b c d -> 0 1
+  for (int i = 0; i < n / 2; i++) {
+    if (s[i] != s[n - 1 - i])
+      return false;
+  }
+  return true;
+}
